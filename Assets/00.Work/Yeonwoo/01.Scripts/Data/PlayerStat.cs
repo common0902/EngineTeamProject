69d0c01bfb1b8606faf1be118db7184f2d2c9a1d
yeonwoo0981 <0981syw@naver.com>
2025-09-27 22:34:07 +0900

    Import Many Assets
    씬 변환 애니메이션, 셰이더, 닷트윈 추가

--- Assets/00.Work/Yeonwoo/01.Scripts/Data/PlayerStat.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerStat", menuName = "Player/PlayerStat")]
-public class PlayerStat : ScriptableObject
-{
-    [SerializeField] private float _test = 100f;
-}
