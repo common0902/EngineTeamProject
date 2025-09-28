72511fd701d23468e2bdb0cf25c704f9f7f7e136
yeonwoo0981 <0981syw@naver.com>
2025-09-28 21:22:25 +0900

    Import TMP
    TextMeshPro 추가

--- Assets/00.Work/Yeonwoo/01.Scripts/InGameCinema.cs
+++ Assets/00.Work/Yeonwoo/01.Scripts/InGameCinema.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class InGameCinema : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _playerSr;
+    [SerializeField] private GameObject _test;
+    [SerializeField] private float _distance = 3f;
+    [SerializeField] private float _duration;
+    private Material material;
+    
+    public event Action ApparanceComplete;
+
+    private void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        material = spriteRenderer.material;
+        _test.transform.position = transform.position;
+    }
 
     private void Start()
     {
-        _playerSr.DOFade(1, 0.8f);
+        PortalAnim();
+    }
+
+    public void PortalAnim()
+    {
+        Vector3 targetPos = _test.transform.position + Vector3.down * _distance;
+        
+        _test.transform.DOMove(targetPos, _duration)
+            .SetEase(Ease.InOutQuint)
+            .OnComplete(() =>
+            {
+                material.DOFloat(0f, "_SourceGlowDissolveFade", 1f)
+                    .SetEase(Ease.OutSine)
+                    .OnComplete(() =>
+                    {
+                        Destroy(gameObject);
+                    });
+                
+                ApparanceComplete?.Invoke();
+            });
     }
 }
