f310bd0bbebb50f5d32d915b4b43b9d7012efd64
songyunhyun <sans10qp10qp@gmail.com>
2025-10-14 16:02:29 +0900

    feat : none

--- Assets/00.Work/SYH/02Script/ETG/SkillPrefab.cs
+++ Assets/00.Work/SYH/02Script/ETG/SkillPrefab.cs
@@ -2,17 +2,8 @@ using UnityEngine;
 
 public abstract class SkillPrefab : MonoBehaviour
 {
-    Transform _target;
-    float _shotSpeed;
-    float _damage;
-    float _duration;
-
-
-    SkillPrefab(Transform target, float shotSpeed, float damage, float duration)
-    {
-        _target = target;
-        _shotSpeed = shotSpeed;
-        _damage = damage;
-        _duration = duration;
-    }
+    public Transform _target;
+    public float _shotSpeed;
+    public float _damage;
+    public float _duration;
 }
