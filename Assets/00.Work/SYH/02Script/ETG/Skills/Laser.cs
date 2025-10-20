82e4cf69955309504d768133bab85c7a7a5cd8e8
songyunhyun <sans10qp10qp@gmail.com>
2025-10-20 23:31:48 +0900

    feat : None

--- Assets/00.Work/SYH/02Script/ETG/Skills/Laser.cs
+++ Assets/00.Work/SYH/02Script/ETG/Skills/Laser.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class Laser : Skill
 {
-    
+    GameObject _prefab;
+    private void Awake()
+    {
+        _prefab = Instantiate(SkillPrefab, Player.Instance.FirePos.position, Quaternion.identity);
+    }
+    public override void Active()
+    {
+        base.Active();
+        _prefab.SetActive(true);
+    }
+    public override void DisActive()
+    {
+        base.DisActive();
+        _prefab.SetActive(false);
+    }
+    protected override void UseSkill()
+    {
+        base.UseSkill();
+        
+    }
 }
--- Assets/00.Work/SYH/02Script/ETG/Skills/LaserPrefab.cs
+++ Assets/00.Work/SYH/02Script/ETG/Skills/LaserPrefab.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserPrefab : SkillPrefab
-{   
-
+{
+    //List<>
+    protected override void Update()
+    {
+        
+    }
 }
