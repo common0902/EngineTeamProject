9fb3095232d74fce8d8d52da65fb4b7b029bd7c7
yeonwoo0981 <0981syw@naver.com>
2025-10-22 23:28:45 +0900

    feat: SkillDataSO
    익숙치 않아서 어렵다..

---
+++ Assets/00.Work/Yeonwoo/01.Scripts/UI/PassiveSkillUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using _00.Work.Yeonwoo._01.Scripts.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _00.Work.Yeonwoo._01.Scripts.UI
+{
+    public class PassiveSkillUI : MonoBehaviour
+    {
+        [SerializeField] private PassiveData passiveData;
+        private TextMeshProUGUI _nameText;
+        private TextMeshProUGUI _descriptionText;
+        private Sprite _sprite;
+
+        private void Awake()
+        {
+            _nameText = GetComponentInChildren<TextMeshProUGUI>();
+            _descriptionText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        private void Start()
+        {
+            _sprite = passiveData.Icon;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            //_nameText = passiveData.Name; + setActive
+            //_descriptionText = passiveData.Description; + setActive
+            // 이거는 내일~~
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            
+        }
+    }
+}
