43902adb4e82c619ff959e4c84e7e0f8c8a82e5c
oshe <shinyjune11@gmail.com>
2025-10-27 10:45:14 +0900

    feat : none

--- Assets/00.Work/AJ/01. Scripts/Enemy/EnemyFSM/EnemyAttack.cs
+++ Assets/00.Work/AJ/01. Scripts/Enemy/EnemyFSM/EnemyAttack.cs
@@ -4,9 +4,11 @@ public class EnemyAttack : MonoBehaviour
 {    
     private EnemyAnimator _enemyAnimator;
     public bool isAnimationEnd = false;
+    private Enemy _enemy;
     private void Awake()
     {
         _enemyAnimator = GetComponentInChildren<EnemyAnimator>();
+        _enemy = GetComponent<Enemy>();
     }
     private void Start()
     {
---
+++ Assets/00.Work/AJ/01. Scripts/PathFind/NodeData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[Serializable]
+public struct NodeData
+{
+    public Vector3 worldPosition;
+    public Vector3Int cellPosition;
+    public List<LinkData> neighbors; // 이웃을 가지고 있어야 하니깐
+
+    public NodeData(Vector3 worldPosition, Vector3Int cellPosition)
+    {
+        this.worldPosition = worldPosition;
+        this.cellPosition = cellPosition;
+        neighbors = new List<LinkData>();
+    }
+
+    public void AddNeighbor(NodeData neighborNode)
+    {
+        neighbors.Add(new LinkData
+        {
+            startPosition = worldPosition,
+            startCellPosition = cellPosition,
+            endPosition = neighborNode.worldPosition,
+            endCellPosition = neighborNode.cellPosition,
+            cost = Vector3Int.Distance(cellPosition, neighborNode.cellPosition)
+        });
+    }
+    
+    public override int GetHashCode() => cellPosition.GetHashCode();
+    
+    public override bool Equals(object obj)
+    {
+        if (obj is NodeData nodeData) // 오브젝트가 노드데이터이면
+        {
+            return nodeData.cellPosition == cellPosition; // 노드 데이터의 cellPos와 나의 cellPos가 같다면
+        }
+
+        return false;
+    }
+
+    public static bool operator ==(NodeData lhs, NodeData rhs) // operator는 예약어, ==을 재정의 한거 
+    {
+        return lhs.Equals(rhs);
+    }
+
+    public static bool operator !=(NodeData lhs, NodeData rhs)
+    {
+        return !(lhs == rhs);
+    }
+}
