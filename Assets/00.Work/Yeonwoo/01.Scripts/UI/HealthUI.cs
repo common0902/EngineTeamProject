e1a402d0ab6cf4a8e7953e276530aca769ba9c71
yeonwoo0981 <0981syw@naver.com>
2025-11-04 23:31:59 +0900

    Garbage Collector
    안쓰는 코드 폐기, 그리고 내일 버그 고쳐야 함

--- Assets/00.Work/Yeonwoo/01.Scripts/UI/HealthUI.cs
+++ Assets/00.Work/Yeonwoo/01.Scripts/UI/HealthUI.cs
@@ -37,7 +37,7 @@ namespace _00.Work.Yeonwoo._01.Scripts.UI
             
             if (_healthSystem is IHealth health) // HealthSystem이 IHealth를 구현했는지 확인함
             {
-                // 굳이 예외 처리를 할 필요는 없지만 안전을 위해서 쓴 코드
+                // 안전장치용 예외처리
                 // 체력바 UI가 변하는걸 자동으로 구독하는 코드
                 _health = health; // 의존성 주입
                 _slider.maxValue = _health.MaxHealth;
