05a155f8c805294190ceb6a0b9244b1c4c011fc2
common0902 <minsoung200@gmail.com>
2025-11-17 09:35:23 +0900

    feat : system

--- Assets/00.Work/PMS/01.Scripts/Door.cs
+++ Assets/00.Work/PMS/01.Scripts/Door.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
+﻿    using UnityEngine;
 
+[RequireComponent (typeof(Animator))]
 public class Door : MonoBehaviour
 {
+    
+
     public Door connectedDoor; // 연결된 반대편 문
     public Vector2Int targetRoomIndex;
     public Vector2Int doorDirection;
+    [HideInInspector] public Room parentRoom;
 
     [SerializeField] private Transform spawnPoint;
+    private bool _isLocked = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !_isLocked)
         {
             TeleportPlayer(collision.gameObject);
         }
@@ -21,7 +26,20 @@ public class Door : MonoBehaviour
         if (connectedDoor != null && connectedDoor.spawnPoint != null)
         {
             player.transform.position = connectedDoor.spawnPoint.position;
+
+            connectedDoor.parentRoom?.OnPlayerEnter();
         }
-        
+    }
+
+    public void Lock()
+    {
+        _isLocked = true;
+        // 문 닫는 애니메이션 재생
+    }
+
+    public void Unlock()
+    {
+        _isLocked = false;
+        // 문 여는 애니메이션 재생
     }
 }
