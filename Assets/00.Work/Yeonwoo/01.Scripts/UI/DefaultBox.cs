5a0e02b8699008482d99ab394d7241ee4f2199e3
yeonwoo0981 <0981syw@naver.com>
2025-11-18 11:06:38 +0900

    feat: box bug

--- Assets/00.Work/Yeonwoo/01.Scripts/UI/DefaultBox.cs
+++ Assets/00.Work/Yeonwoo/01.Scripts/UI/DefaultBox.cs
@@ -6,10 +6,30 @@ namespace _00.Work.Yeonwoo._01.Scripts.UI
 {
     public class DefaultBox : AbstractBox
     {
-        protected override void BoxOpenAnim()
+        [SerializeField] private List<GameObject> items = new List<GameObject>();
+        
+        protected override void ItemScattering()
         {
-            base.BoxOpenAnim();
-            Debug.Log("전투 승리 박스 오픈");
+            if (items == null || items.Count == 0)
+            {
+                Debug.LogWarning("아이템 리스트가 비어 있습니다."); 
+                return;
+            }
+            
+            GameObject selectedItem = items[Random.Range(0, items.Count)];
+            GameObject drop = Instantiate(selectedItem, transform.position, Quaternion.identity);
+            
+            drop.transform.localScale = Vector3.zero;
+            
+            Vector2 randomDIr = Random.insideUnitCircle.normalized;
+
+            Vector3 targetPos = transform.position + (Vector3)(randomDIr * ScatteringRange);
+            
+            Sequence seq = DOTween.Sequence();
+
+            seq.Append(drop.transform.DOScale(Vector3.one, 0.5f))
+                .Append(drop.transform.DOMove(targetPos, 0.7f))
+                .SetEase(Ease.OutQuad);
         }
     }
 }
