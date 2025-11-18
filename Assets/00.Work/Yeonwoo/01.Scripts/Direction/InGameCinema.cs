d000be10cad4897345065a765e8b9e0584c6a0a7
yeonwoo0981 <0981syw@naver.com>
2025-11-18 16:09:27 +0900

    feat: 용사, 닥쳐라 (i am sorry) 사랑하는 사람? (Don't leave me) 짐은 늙지도 죽지도 않아서 (I want you here with me) 이미 그 들은 (I know that) 가슴에 묻은지 오래다 (your love is gone
    야이 맛국노야 설마 2015년 7월 21일 새벽에 배우 채수빈이 구자욱이랑 거리에서 손잡고 걸어다니는 모습이 찍혀 열애설이 터지고 구자욱은 열애설을 인정했지만 채수빈은 "맛보기한 느낌이랄까? 열애설이 이런 기분이구나" 라고 말해 구자욱이 맛자욱이 된 그 사건을 말하는 것이냐 Hey, Matgukno, don't tell me that actor Chae Soo-bin was photographed walking hand in hand with Koo Ja-wook on the street at dawn on July 21, 2015, and rumors of a romantic relationship broke out, and Koo Ja-wook admitted to the romantic relationship, but Chae Soo-bin said, "Should I say it's like a taste? This is how the romantic relationship feels." Are you talking about the incident in which Koo Ja-wook became a taste
    おい、まさか2015年7月21日未明に俳優のチェ·スビンがク·ジャウクと街で手をつないで歩く姿が撮られ、熱愛説が浮上し、ク·ジャウクは熱愛説を認めたが、チェ·スビンは「味見した感じというか？ 熱愛説がこんな気分なんだな」と言い、ク·ジャウクがマッジャウクになった事件を言うのですか 呀，这味国奴呀，2015年7月21日凌晨，演员蔡秀彬和具滋旭在大街上手牵手走路的样子被拍到，爆出恋爱传闻，具滋旭承认恋爱传闻，但蔡秀彬却说"感觉像是尝鲜一样？ 原来恋爱传闻就是这种心情啊"，是具滋旭被炒得沸沸扬扬的事件吗？Yai Guk-guk-no, el 21 de julio de 2015, la actriz Chae Soobin fue vista caminando de la mano con Koo Ja-wook en la calle, explotando los rumores de una relación romántica, y Koo Ja-wook admitió el rumor de una relación romántica, pero Chae Soo-bin admitió: "¿Soobin? Así es como se sienten los rumores de una relación romántica." ¿Te refieres al incidente en el que Koo Ja-wook se convirtió en un adicto? S'il vous plaît, ne me dites pas que l'acteur Chae Soo-bin a été vu marcher main dans la rue avec Koo Ja-wook à l'aube du 21 juillet 2015, et que Koo Ja-wook a admis qu'il avait une relation romantique. Tu veux dire que tu as une histoire d'amour romantique et que tu penses à l'histoire de Koo Ja-wook qui est devenu un gouter ? Am 21. Juli 2015, als der Geschmack von Yae Mi-na war, zeigte sich, dass Chae Soo-bin mit Koo Ja-wook auf der Straße herumging. "Das ist das Gefühl, das du hast." "Das ist das, was Gujaruk als "Geschmacks-Junkie" bezeichnet." Эй, Макуккук Ноя, может быть? 21 июля 2015 года ранним утром на улице появились слухи о том, что актер Ча Су Бин ходит рука об руку с Гу Джауком, что привело к слухам о романтических отношениях, но Чэ Су Бин сказала: «Это похоже на вкус? Вы говорите, что слухи о романтических отношениях выглядят так". Вы имеете в виду инцидент, в котором Ку Джа Ук стал таким вкусным? في صباح يوم 21 يوليو 2015 ، تم القبض على شاب يدعى جاكي شان وهو يمشي في الشارع ، مما أثار جدلا كبيرا حول ما إذا كان بإمكانه أن يقول: "هل أنت بخير؟" أم لا؟ لا. قوله تعالى: {وإذا جاءكم فاسألوا أهل الذكر إن كنتم لا تعلمون} الآية.

--- Assets/00.Work/Yeonwoo/01.Scripts/Direction/InGameCinema.cs
+++ Assets/00.Work/Yeonwoo/01.Scripts/Direction/InGameCinema.cs
@@ -6,48 +6,22 @@ namespace _00.Work.Yeonwoo._01.Scripts.Direction
 {
     public class InGameCinema : MonoBehaviour
     {
-        private AppearancePlayerVisual visualChange;
-        private AppearancePlayer player;
-        [SerializeField] private float distance = 3f;
-        [SerializeField] private float duration;
-        private Material material;
-    
-        public event Action AppearanceComplete;
-
+        private Material _material;
+        
         private void OnEnable()
         {
-            visualChange = GameObject.Find("Player").GetComponentInChildren<AppearancePlayerVisual>();
-            player = GameObject.Find("Player").GetComponent<AppearancePlayer>();
             SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-            material = spriteRenderer.material;
-            player.transform.position = transform.position;
-            visualChange.PlayerScaleChanged += PortalAnim;
-        }
-
-        private void PortalAnim()
-        {
-            Vector3 targetPos = player.transform.position + Vector3.down * distance;
-        
-            player.transform.DOMove(targetPos, duration)
-                .SetEase(Ease.InOutQuint)
-                .OnComplete(() =>
-                {
-                    AppearanceComplete?.Invoke();
-                    DOVirtual.DelayedCall(1.5f, () =>
+            _material = spriteRenderer.material;
+            
+            DOVirtual.DelayedCall(6.5f, () =>
+            {
+                _material.DOFloat(0f, "_FullGlowDissolveFade", 2f)
+                    .SetEase(Ease.OutSine)
+                    .OnComplete(() =>
                     {
-                        material.DOFloat(0f, "_FullGlowDissolveFade", 2f)
-                            .SetEase(Ease.OutSine)
-                            .OnComplete(() =>
-                            {
-                                Destroy(gameObject);
-                            });
+                        Destroy(gameObject);
                     });
-                });
-        }
-
-        private void OnDisable()
-        {
-            visualChange.PlayerScaleChanged -= PortalAnim;
+            });
         }
     }
 }
