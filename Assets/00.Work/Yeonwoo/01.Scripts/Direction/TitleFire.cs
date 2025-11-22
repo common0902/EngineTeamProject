a7f02869a5506041def0123d0b8938f73aa226fe
yeonwoo0981 <0981syw@naver.com>
2025-11-22 17:58:27 +0900

    feat: TitleStartEndingDeadBoxOpen CutScene / SkillDash Cooltime UI - 7

---
+++ Assets/00.Work/Yeonwoo/01.Scripts/Direction/TitleFire.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using _00.Work.Yeonwoo._01.Scripts.UI;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _00.Work.Yeonwoo._01.Scripts.Direction
+{
+    public class TitleFire : MonoBehaviour
+    {
+        [SerializeField] private SettingSystem settingSystem;
+        private RectTransform _thisTransform;
+
+        private void Awake()
+        {
+            _thisTransform = GetComponent<RectTransform>();
+        }
+
+        private void OnEnable()
+        {
+            settingSystem.OnStartButtonClick += StartEvent;
+        }
+
+        private void StartEvent()
+        {
+            _thisTransform.DOSizeDelta(new Vector2(8500f, 8500f), 3f)
+                .SetEase(Ease.OutQuad)
+                .OnComplete(() =>
+                {
+                    SceneManager.LoadScene("Develop");
+                });
+        }
+        
+        private void OnDisable()
+        {
+            settingSystem.OnStartButtonClick -= StartEvent;
+        }
+    }
+}
