e0206dd94697ba9fadc7812b265822625206ce5b
common0902 <minsoung200@gmail.com>
2025-11-23 15:29:18 +0900

    feat : RoomType

---
+++ Assets/00.Work/PMS/01.Scripts/BossRoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BossRoom : Room
+{
+
+    protected override void Start()
+    {
+        base.Start();
+        hasEnemies = true;
+        enemyCount = 1;   // 보스 1마리
+    }
+
+    public override void OnPlayerEnter()
+    {
+        // 기본 Room 입장 로직(문 잠금 등) 먼저 실행
+        base.OnPlayerEnter();
+
+       
+    }
+
+    private void HandleBossDied()
+    {
+        OnEnemyDied();
+    }
+
+    public override void OnEnemyDied()
+    {
+        base.OnEnemyDied();
+    }
+}
---
+++ Assets/00.Work/PMS/01.Scripts/StartRoom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class StartRoom : Room
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void OnPlayerEnter()
+    {
+        base.OnPlayerEnter();
+    }
+}
