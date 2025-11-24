01afb130c290de5b04a81274552ae8efc92d3669
songyunhyun <sans10qp10qp@gmail.com>
2025-11-24 22:51:24 +0900

    fix : usingDelete

--- Assets/00.Work/SYH/02Script/ETG/SkillUtility.cs
+++ Assets/00.Work/SYH/02Script/ETG/SkillUtility.cs
@@ -15,27 +15,17 @@ public class SkillUtility
         pos.transform.localScale = desireAngle > 90 || desireAngle < -90 ? new Vector3(pos.localScale.x, -pos.localScale.y, 1) : new Vector3(pos.localScale.x, pos.localScale.y, 1);
         pos.rotation = Quaternion.Euler(0, 0, desireAngle);
         return (MouseInput.Instance.MousePosition - (Vector2)pos.transform.position).normalized;
-        //if (desireAngle > 45 && desireAngle < 135)
-        //{
-        //    pos.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = -1;
-        //}
-        //else
-        //{
-        //    pos.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1;
-        //}
     }
     public static Vector2 AimWeapon(Vector2 pos)
     {
         return (MouseInput.Instance.MousePosition - pos).normalized;
     }
-    public static IEnumerator PastDelay(Skill type, float value) // �� ��
+    public static IEnumerator PastDelay(Skill type, float value)
     {
         OnEndPastDelay += type.EndPastDelay;
         Player.Instance.PlayerMoveCompo._cannotMove = false;
-        //Player.Instance.SkillControllerCompo._canUseSkill = false;
         yield return new WaitForSeconds(value);
         Player.Instance.PlayerMoveCompo._cannotMove = true;
-        //Player.Instance.SkillControllerCompo._canUseSkill = true;
         OnEndPastDelay?.Invoke();
     }
     public static float CalcurateDamage(float dmg)
